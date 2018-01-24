4c0a11bfab5850b84dc8b615d93620fbc28e49f5
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-01-24 15:36:05 +0700

    Additional user-id param

--- public/include/analytics/ga.php
+++ public/include/analytics/ga.php
@@ -5,14 +5,18 @@
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-112810670-1');
+  gtag('config', 'UA-112810670-1', {
+    'custom_map': {'dimension1': 'bbcg-user-id'}
+  });
 </script>
 <? if ($USER->IsAuthorized()): ?>
     <script>
         gtag('set', {'user_id': <? echo $USER->GetID() ?>}); 
+        gtag('event', 'user_id_dimension', {'bbcg-user-id': <? echo $USER->GetID() ?>});
     </script>
 <? elseif (isset($_GET['user_id'])): ?>
     <script>
-        gtag('set', {'user_id': <? echo htmlspecialchars($_GET['user_id']) ?>}); 
+        gtag('set', {'user_id': <? echo htmlspecialchars($_GET['user_id']) ?>});
+        gtag('event', 'user_id_dimension', {'bbcg-user-id': <? echo htmlspecialchars($_GET['user_id']) ?>});
     </script>
 <? endif ?>
