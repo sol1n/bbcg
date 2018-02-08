06588b537fff03efa1b72ce4494b5df8389ca490
alexeynechaev <30683342+alexeynechaev@users.noreply.github.com>
2018-02-08 13:27:03 +0700

    Update template.php

--- public/bitrix/templates/.default/components/bitrix/news.detail/summit/lang/ru/template.php
+++ public/bitrix/templates/.default/components/bitrix/news.detail/summit/lang/ru/template.php
@@ -8,9 +8,9 @@
 	$MESS["EMAIL"] = "E-mail";
 	$MESS["COMPANY"] = "Компания";
 	$MESS["POSITION"] = "Должность";
-	$MESS["WE_WILL_CONTACT_YOU"] = "С вами свяжется NAME, для уточнения деталей в течение 2-х дней";
+	$MESS["WE_WILL_CONTACT_YOU"] = "С вами свяжется NAME для уточнения деталей в течение двух минут";
 	$MESS["ALTERNATIVE_REGISTRATION"] = "Альтернативные способы регистрации";
 	$MESS["OR"] = "или";
-	$MESS["CONTACT_PERSON"] = "контактное лицо Ирина Чиннова";
+	$MESS["CONTACT_PERSON"] = "Контактное лицо Ирина Чиннова";
 	$MESS["DOWNLOAD_AND_SEND_REQUEST"] = "Скачать заявку и отправить на указанную почту";
 ?>
