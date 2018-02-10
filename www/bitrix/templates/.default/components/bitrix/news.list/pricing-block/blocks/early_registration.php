1d4772209fa1d93d5fb72d7cf70d34ba3833194d
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-02-10 17:18:30 +0700

    Pricing block without early registration

---
+++ www/bitrix/templates/.default/components/bitrix/news.list/pricing-block/blocks/early_registration.php
@@ -0,0 +1,82 @@
+<? 
+    use \Bitrix\Main\Localization\Loc;
+?>
+<? if ($arResult['ITEMS']): ?>
+    <section class="summit-price-block">
+        <div class="wrapper">
+            <div class="summit-price-block-title">
+                <?=Loc::GetMessage('PARTICIPATE_PRICING', [], $arParams['LANG'])?>
+            </div>
+
+            <div class="summit-price-block-row">
+                <div class="summit-price-block-left">
+                    <div style="overflow-x: auto">
+                        <table class="summit-price-block-table">
+                            <tr class="summit-price-block-table-heading">
+                                <th>
+                                    <div class="summit-price-block-table-title">
+                                        <?=Loc::GetMessage('REGISTRATION_TYPE', [], $arParams['LANG'])?>
+                                    </div>
+                                </th>
+                                <td>
+                                    <div class="summit-price-block-table-title">
+                                        <?=Loc::GetMessage('EARLY_REGISTRATION', [], $arParams['LANG'])?>
+                                    </div>
+                                    <? if ($arResult['EARLY_REGISTRATION']): ?>
+                                        <div class="summit-price-block-table-subtitle">
+                                            <?=Loc::GetMessage('BEFORE', [], $arParams['LANG'])?> <?=$arResult['EARLY_REGISTRATION']?>
+                                        </div>
+                                    <? endif ?>
+                                </td>
+                                <td>
+                                    <div class="summit-price-block-table-title">
+                                        <?=Loc::GetMessage('LATE_REGISTRATION', [], $arParams['LANG'])?>
+                                    </div>
+                                    <? if ($arResult['EARLY_REGISTRATION']): ?>
+                                        <div class="summit-price-block-table-subtitle">
+                                            <?=Loc::GetMessage('AFTER', [], $arParams['LANG'])?> <?=$arResult['EARLY_REGISTRATION']?>
+                                        </div>
+                                    <? endif ?>
+                                </td>
+                            </tr>
+                            <? foreach ($arResult['ITEMS'] as $item): ?>
+                                <tr>
+                                    <th>
+                                        <div class="summit-price-block-table-border">
+                                            <?=$item['NAME']?>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div class="summit-price-block-table-border">
+                                            <span class="summit-price-block-table-value">
+                                                <?=$item['PROPERTIES']['BEFORE']['VALUE']?>
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="summit-price-block-table-border">
+                                            <span class="summit-price-block-table-value">
+                                                <?=$item['PROPERTIES']['AFTER']['VALUE']?>
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <? endforeach ?>
+                            <tr>
+                                <th>&nbsp;</th>
+                                <td colspan="2">
+                                    <div class="summit-price-block-table-charge">
+                                        <?=Loc::GetMessage('PRICE_SPECIFIED_WITH_NDS', [], $arParams['LANG'])?>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="summit-price-block-right">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+<? endif ?>
---
+++ www/bitrix/templates/.default/components/bitrix/news.list/pricing-block/blocks/simple.php
@@ -0,0 +1,60 @@
+<? 
+    use \Bitrix\Main\Localization\Loc;
+?>
+<? if ($arResult['ITEMS']): ?>
+    <section class="summit-price-block">
+        <div class="wrapper">
+            <div class="summit-price-block-title">
+                <?=Loc::GetMessage('PARTICIPATE_PRICING', [], $arParams['LANG'])?>
+            </div>
+
+            <div class="summit-price-block-row">
+                <div class="summit-price-block-left">
+                    <div style="overflow-x: auto">
+                        <table class="summit-price-block-table">
+                            <tr class="summit-price-block-table-heading">
+                                <th>
+                                    <div class="summit-price-block-table-title">
+                                        <?=Loc::GetMessage('REGISTRATION_TYPE', [], $arParams['LANG'])?>
+                                    </div>
+                                </th>
+                                <td>
+                                    <div class="summit-price-block-table-title">
+                                        <?=Loc::GetMessage('REGISTRATION', [], $arParams['LANG'])?>
+                                    </div>
+                                </td>
+                            </tr>
+                            <? foreach ($arResult['ITEMS'] as $item): ?>
+                                <tr>
+                                    <th>
+                                        <div class="summit-price-block-table-border">
+                                            <?=$item['NAME']?>
+                                        </div>
+                                    </th>
+                                    <td>
+                                        <div class="summit-price-block-table-border">
+                                            <span class="summit-price-block-table-value">
+                                                <?=$item['PROPERTIES']['BEFORE']['VALUE']?>
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <? endforeach ?>
+                            <tr>
+                                <th>&nbsp;</th>
+                                <td colspan="2">
+                                    <div class="summit-price-block-table-charge">
+                                        <?=Loc::GetMessage('PRICE_SPECIFIED_WITH_NDS', [], $arParams['LANG'])?>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div class="summit-price-block-right">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+<? endif ?>
