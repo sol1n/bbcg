63969771b95f16929b0140a4166d7241fcaad84a
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-03-27 12:18:53 +0700

    Removing year from events old link redirect

--- www/en/events/redirect.php
+++ www/en/events/redirect.php
@@ -5,9 +5,18 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_befo
 CModule::IncludeModule('iblock');
 
 if (isset($_GET['code']) && !empty($_GET['code'])) {
+	$code = $_GET['code'];
+	if (mb_strpos($code, '-') !== false) {
+		$array = explode('-', $code);
+		if (is_array($array) && is_numeric($array[count($array) - 1])) {
+			unset($array[count($array) - 1]);
+			$code = implode('-', $array);
+		}
+	}
+
 	$res = CIblockElement::GetList(
 		['ID' => 'ASC'],
-		['IBLOCK_ID' => SUMMITS_IBLOCK, 'CODE' => $_GET['code']],
+		['IBLOCK_ID' => SUMMITS_IBLOCK, 'CODE' => $code, 'ACTIVE' => 'Y'],
 		false,
 		false,
 		['ID', 'NAME', 'CODE']
