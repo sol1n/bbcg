6123a195db089a2a63e573728fb5b148e208092f
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-04-12 11:58:18 +0700

    Vote page

---
+++ www/vote/index.php
@@ -0,0 +1,17 @@
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+?> 
+
+<div class="wrapper">
+    <div class="m-t-lg">
+        <div class="registration-modal-title">
+            Голосование
+        </div>
+
+        <iframe src="/templates/vote.php" width="100%" height="300" frameBorder="0" ></iframe>
+    </div>
+</div>
+
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
