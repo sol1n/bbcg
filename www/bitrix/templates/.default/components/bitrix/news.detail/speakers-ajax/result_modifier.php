63c445b2cfe4c5bbb054cb7553cad0ce6113d3cc
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-05-14 15:09:48 +0700

    Speaker modal time format changed

--- www/bitrix/templates/.default/components/bitrix/news.detail/speakers-ajax/result_modifier.php
+++ www/bitrix/templates/.default/components/bitrix/news.detail/speakers-ajax/result_modifier.php
@@ -6,14 +6,14 @@
     while ($event = $res->Fetch()) {
     	if ($arParams['LANG'] == 'en') {
     		$event['DETAIL_PAGE_URL'] = '/en/' . $event['PROPERTY_SUMMIT_CODE'] . '/events/' . $event['ID'] . '/';
-    		$event['DATE'] = mb_strtolower(PHPFormatDateTime($event['PROPERTY_BEGIN_VALUE'], 'j F, h:i'));
+    		$event['DATE'] = mb_strtolower(PHPFormatDateTime($event['PROPERTY_BEGIN_VALUE'], 'j F, G:i'));
     		$event['NAME'] = !empty($event['PROPERTY_EN_NAME_VALUE']) ? $event['PROPERTY_EN_NAME_VALUE'] : $event['NAME'];
     		$event['PROPERTY_AREA_NAME'] = !empty($event['PROPERTY_AREA_PROPERTY_EN_NAME_VALUE'])
     			? $event['PROPERTY_AREA_PROPERTY_EN_NAME_VALUE']
     			: $event['PROPERTY_AREA_NAME'];
     	} else {
     		$event['DETAIL_PAGE_URL'] = '/' . $event['PROPERTY_SUMMIT_CODE'] . '/events/' . $event['ID'] . '/';
-    		$event['DATE'] = mb_strtolower(FormatDate('j F, h:i', MakeTimeStamp($event['PROPERTY_BEGIN_VALUE'], "DD.MM.YYYY HH:MI:SS")));
+    		$event['DATE'] = mb_strtolower(FormatDate('j F, G:i', MakeTimeStamp($event['PROPERTY_BEGIN_VALUE'], "DD.MM.YYYY HH:MI:SS")));
     	}
         if (isset($arResult['EVENTS'][$event['ID']]['PROPERTY_AREA_NAME'])) {
             $event['PROPERTY_AREA_NAME'] = $arResult['EVENTS'][$event['ID']]['PROPERTY_AREA_NAME'] . ', ' . $event['PROPERTY_AREA_NAME'];
