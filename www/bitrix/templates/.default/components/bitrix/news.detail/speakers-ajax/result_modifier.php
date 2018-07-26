108c77233c8a07e748f388d07b4a943ca481ba26
Unknown <dr.nightingale@mail.ru>
2018-07-26 19:14:37 +0800

    Hide past session on speaker page and modal

--- www/bitrix/templates/.default/components/bitrix/news.detail/speakers-ajax/result_modifier.php
+++ www/bitrix/templates/.default/components/bitrix/news.detail/speakers-ajax/result_modifier.php
@@ -1,8 +1,8 @@
 <?
     CModule::IncludeModule('iblock');
-
-    $selected = ['ID', 'NAME', 'PROPERTY_BEGIN', 'PROPERTY_EN_NAME', 'PROPERTY_AREA.NAME', 'PROPERTY_AREA.PROPERTY_EN_NAME', 'PROPERTY_SUMMIT.CODE'];
-    $res = CIBlockElement::GetList(['PROPERTY_BEGIN' => 'DESC'], ['IBLOCK_ID' => EVENTS_IBLOCK, 'PROPERTY_SPEAKERS' => $arResult['ID']], false, false, $selected);
+    $now_date = date("Y-m-d H:i:s");
+    $selected = ['ID', 'NAME', 'PROPERTY_BEGIN', 'PROPERTY_END', 'PROPERTY_EN_NAME', 'PROPERTY_AREA.NAME', 'PROPERTY_AREA.PROPERTY_EN_NAME', 'PROPERTY_SUMMIT.CODE'];
+    $res = CIBlockElement::GetList(['PROPERTY_BEGIN' => 'DESC'], ['IBLOCK_ID' => EVENTS_IBLOCK, 'PROPERTY_SPEAKERS' => $arResult['ID'], '>PROPERTY_END' => $now_date], false, false, $selected);
     while ($event = $res->Fetch()) {
     	if ($arParams['LANG'] == 'en') {
     		$event['DETAIL_PAGE_URL'] = '/en/' . $event['PROPERTY_SUMMIT_CODE'] . '/events/' . $event['ID'] . '/';
@@ -22,12 +22,12 @@
     }
 
     if ($arParams['LANG'] == 'en') {
-        $arResult['NAME'] = !empty($arResult['PROPERTIES']['EN_NAME']['VALUE']) ? $arResult['PROPERTIES']['EN_NAME']['VALUE'] : $arResult['NAME']; 
-        $arResult['~PREVIEW_TEXT'] = !empty($arResult['PROPERTIES']['EN_PREVIEW_TEXT']['~VALUE']['TEXT']) 
-        	? $arResult['PROPERTIES']['EN_PREVIEW_TEXT']['~VALUE']['TEXT'] 
+        $arResult['NAME'] = !empty($arResult['PROPERTIES']['EN_NAME']['VALUE']) ? $arResult['PROPERTIES']['EN_NAME']['VALUE'] : $arResult['NAME'];
+        $arResult['~PREVIEW_TEXT'] = !empty($arResult['PROPERTIES']['EN_PREVIEW_TEXT']['~VALUE']['TEXT'])
+        	? $arResult['PROPERTIES']['EN_PREVIEW_TEXT']['~VALUE']['TEXT']
         	: $arResult['~PREVIEW_TEXT'];
-        $arResult['~DETAIL_TEXT'] = !empty($arResult['PROPERTIES']['EN_DETAIL_TEXT']['~VALUE']['TEXT']) 
-        	? $arResult['PROPERTIES']['EN_DETAIL_TEXT']['~VALUE']['TEXT'] 
+        $arResult['~DETAIL_TEXT'] = !empty($arResult['PROPERTIES']['EN_DETAIL_TEXT']['~VALUE']['TEXT'])
+        	? $arResult['PROPERTIES']['EN_DETAIL_TEXT']['~VALUE']['TEXT']
         	: $arResult['~DETAIL_TEXT'];
 	}
 ?>
