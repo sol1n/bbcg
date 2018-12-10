81d55c3e3022c926ee0a6115b7f4834148910f10
alexeynechaev <30683342+alexeynechaev@users.noreply.github.com>
2018-12-10 15:26:50 +0700

    Update banner.php

--- www/include/banners/banner.php
+++ www/include/banners/banner.php
@@ -4,14 +4,14 @@
         <div class="row">
             <div class="col-md-2 col-sm-2 banner-left">
                 <a href="/exclusive/makeretailgreatagain/" target="_blank">
-                    <img src="/assets/images/banner-element.png"/>
+                    <img src="/assets/images/banner-element-speakers.jpg"/>
                 </a>
             </div>
             <div class="col-md-8 col-sm-8 banner-center">
-                <img src="/assets/images/banner-central.png"/>
+                <img src="/assets/images/banner-central-speakers.jpg"/>
             </div>
             <div class="col-md-2 col-sm-2 banner-right">
-                <a class="banner-button button button-light-burgundy" href="/exclusive/makeretailgreatagain/" target="_blank">ЧИТАТЬ ЖУРНАЛ</a>
+                <a class="banner-button button button-light-burgundy" href="/speakers-contest/" target="_blank">ХОЧУ ПОБЕДИТЬ!</a>
             </div>
         </div>
     </div>
