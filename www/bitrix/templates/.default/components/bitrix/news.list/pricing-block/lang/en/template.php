2c96e9826816b733b1db06bee3cf03c69fab2ad8
alexeynechaev <a.nechaev@digitalsparta.com>
2018-12-18 14:09:44 +0700

    Nds change

--- www/bitrix/templates/.default/components/bitrix/news.list/pricing-block/lang/en/template.php
+++ www/bitrix/templates/.default/components/bitrix/news.list/pricing-block/lang/en/template.php
@@ -5,7 +5,7 @@
 	$MESS["BEFORE"] = "before";
 	$MESS["LATE_REGISTRATION"] = "Late registration";
 	$MESS["AFTER"] = "after";
-	$MESS["PRICE_SPECIFIED_WITH_NDS"] = "Prices include VAT 18%.";
+	$MESS["PRICE_SPECIFIED_WITH_NDS"] = "Prices include VAT 20%.";
 	$MESS["TRADITIONAL_DISCOUNTS"] = "BBCG provides traditional discounts for permanent participants, and when registering 2 or more representatives of the company.";
 	$MESS["ADDITIONAL_DISCOUNT"] = "In the case of providing a description of 1 implemented business optimization solution - an additional discount 15%.";
 ?>
