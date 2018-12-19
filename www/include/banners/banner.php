1ef47c71e6ac4a327813622091806652b9eb4d3e
alexeynechaev <a.nechaev@digitalsparta.com>
2018-12-19 18:26:33 +0700

    Banner food

--- www/include/banners/banner.php
+++ www/include/banners/banner.php
@@ -3,15 +3,15 @@
     <div class="wrapper">
         <div class="row">
             <div class="col-md-2 col-sm-2 banner-left">
-                <a href="/speakers-contest/?utm_source=Vezdehod&utm_medium=Banner&utm_campaign=10_12" target="_blank">
-                    <img src="/assets/images/banner-element-speakers.jpg"/>
+                <a href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">
+                    <img src="/assets/images/banner/food-1.jpg"/>
                 </a>
             </div>
             <div class="col-md-8 col-sm-8 banner-center">
-                <img src="/assets/images/banner-central-speakers.jpg"/>
+                <img src="/assets/images/banner/food-2.jpg"/>
             </div>
             <div class="col-md-2 col-sm-2 banner-right">
-                <a class="banner-button button button-light-burgundy" href="/speakers-contest/?utm_source=Vezdehod&utm_medium=Banner&utm_campaign=10_12" target="_blank">ХОЧУ ПОБЕДИТЬ!</a>
+                <a class="banner-button button button-light-burgundy" href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
             </div>
         </div>
     </div>
