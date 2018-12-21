fcf98b70e51841c5f4c2d85be66b037f35b73f0c
alexeynechaev <a.nechaev@digitalsparta.com>
2018-12-21 12:24:40 +0700

    Banned magaz

--- www/include/banners/banner.php
+++ www/include/banners/banner.php
@@ -3,15 +3,15 @@
     <div class="wrapper">
         <div class="row">
             <div class="col-md-2 col-sm-2 banner-left">
-                <a href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">
-                    <img src="/assets/images/banner/food-1.jpg"/>
+                <a href="/shop-forever/?utm_source=banner&utm_medium=MF&utm_campaign=19_12#summit-registration-block" target="_blank">
+                    <img src="/assets/images/banner/magaz-1.jpg"/>
                 </a>
             </div>
             <div class="col-md-8 col-sm-8 banner-center">
-                <img src="/assets/images/banner/food-2.jpg"/>
+                <img src="/assets/images/banner/magaz-2.jpg"/>
             </div>
             <div class="col-md-2 col-sm-2 banner-right">
-                <a class="banner-button button button-light-burgundy" href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
+                <a class="banner-button button button-light-burgundy" href="/shop-forever/?utm_source=banner&utm_medium=MF&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
             </div>
         </div>
     </div>
