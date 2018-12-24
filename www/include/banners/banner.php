27524b9ee7d12155e9b6e12d9d6ead66833db844
alexeynechaev <a.nechaev@digitalsparta.com>
2018-12-24 12:24:46 +0700

    banner-orr

--- www/include/banners/banner.php
+++ www/include/banners/banner.php
@@ -3,15 +3,15 @@
     <div class="wrapper">
         <div class="row">
             <div class="col-md-2 col-sm-2 banner-left">
-                <a href="/shop-forever/?utm_source=banner&utm_medium=MF&utm_campaign=19_12#summit-registration-block" target="_blank">
-                    <img src="/assets/images/banner/magaz-1.jpg"/>
+                <a href="/online-retail-russia-2019/?utm_source=banner&utm_medium=ORR&utm_campaign=19_12#summit-registration-block" target="_blank">
+                    <img src="/assets/images/banner/orr-1.jpg"/>
                 </a>
             </div>
             <div class="col-md-8 col-sm-8 banner-center">
-                <img src="/assets/images/banner/magaz-2.jpg"/>
+                <img src="/assets/images/banner/orr-2.jpg"/>
             </div>
             <div class="col-md-2 col-sm-2 banner-right">
-                <a class="banner-button button button-light-burgundy" href="/shop-forever/?utm_source=banner&utm_medium=MF&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
+                <a class="banner-button button button-light-burgundy" href="/online-retail-russia-2019/?utm_source=banner&utm_medium=ORR&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
             </div>
         </div>
     </div>
