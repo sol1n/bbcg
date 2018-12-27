9ce6b2c44925c3f1324719d610182d028d8c59e0
alexeynechaev <a.nechaev@digitalsparta.com>
2018-12-27 12:28:41 +0700

    banner-food

--- www/include/banners/banner.php
+++ www/include/banners/banner.php
@@ -3,15 +3,15 @@
     <div class="wrapper">
         <div class="row">
             <div class="col-md-2 col-sm-2 banner-left">
-                <a href="/diy-household-retail-russia-2019/?utm_source=banner&utm_medium=DIY&utm_campaign=19_12#summit-registration-block" target="_blank">
-                    <img src="/assets/images/banner/diy-1.jpg"/>
+                <a href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">
+                    <img src="/assets/images/banner/food-1.jpg"/>
                 </a>
             </div>
             <div class="col-md-8 col-sm-8 banner-center">
-                <img src="/assets/images/banner/diy-2.jpg"/>
+                <img src="/assets/images/banner/food-2.jpg"/>
             </div>
             <div class="col-md-2 col-sm-2 banner-right">
-                <a class="banner-button button button-light-burgundy" href="/diy-household-retail-russia-2019/?utm_source=banner&utm_medium=DIY&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
+                <a class="banner-button button button-light-burgundy" href="/food-business-russia-2019/?utm_source=banner&utm_medium=Food&utm_campaign=19_12#summit-registration-block" target="_blank">РЕГИСТРАЦИЯ</a>
             </div>
         </div>
     </div>
