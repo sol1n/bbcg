170c99bbe0f971c394f6013784b0cb228b85c6fd
Unknown <dr.nightingale@mail.ru>
2019-03-20 19:08:54 +0800

    use 'REQUEST_URI'

--- www/summits/redirect.php
+++ www/summits/redirect.php
@@ -4,15 +4,12 @@ global $USER;
 if ($USER->IsAdmin()){
     $user_ID = $USER->GetID();
     if($user_ID=='182'){
-        echo '<pre>';
-        print_r($_SERVER);
-        echo '</pre>';
-        $haystack = $_SERVER['DOCUMENT_URI'];
+        $haystack = $_SERVER['REQUEST_URI'];
         $needle = '20';
         $pos = strripos($haystack, $needle);
 
         if ($pos === false) {
-            $link = explode('/', $_SERVER['DOCUMENT_URI'], 3);
+            $link = explode('/', $_SERVER['REQUEST_URI'], 3);
             $new_link_start = $link[1];
             $new_link_end = $link[2];
             $new_link = "/".$new_link_start."-2019/".$new_link_end;
