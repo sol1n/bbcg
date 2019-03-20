33148282465c3f479f38a2313d324b09fc709914
Unknown <dr.nightingale@mail.ru>
2019-03-20 18:20:12 +0800

    /jquery_migrate_test: Auto stash before merge of "userstory/jquery_migrate_test" and "master"
    add redirect /*summit-name*/ to /*summit-name-2019*/

---
+++ www/summits/events/redirect.php
@@ -0,0 +1,12 @@
+<? //если год саммита не указан в url перенаправлять на страницу текущего года
+    $haystack = $_SERVER['DOCUMENT_URI'];
+    $needle = '20';
+
+    $pos = strripos($haystack, $needle);
+    if ($pos === false) {
+        $link = explode('/', $_SERVER['DOCUMENT_URI'], 3);
+        $new_link_start = $link[1];
+        $new_link_end = $link[2];
+        LocalRedirect("/".$new_link_start."-2019/".$new_link_end);
+    }
+?>
