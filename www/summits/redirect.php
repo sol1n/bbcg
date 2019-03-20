2444f8562b19118d65bacde2560a8bf734741e50
Unknown <dr.nightingale@mail.ru>
2019-03-20 19:16:49 +0800

    fix redirect

--- www/summits/redirect.php
+++ www/summits/redirect.php
@@ -1,25 +1,15 @@
-<? //если год саммита не указан в url перенаправлять на страницу текущего года
+<?
+//если год саммита не указан в url перенаправлять на страницу текущего года
 
-global $USER;
-if ($USER->IsAdmin()){
-    $user_ID = $USER->GetID();
-    if($user_ID=='182'){
-        $haystack = $_SERVER['REQUEST_URI'];
-        $needle = '20';
-        $pos = strripos($haystack, $needle);
+$haystack = $_SERVER['REQUEST_URI'];
+$needle = '20';
+$pos = strripos($haystack, $needle);
 
-        if ($pos === false) {
-            $link = explode('/', $_SERVER['REQUEST_URI'], 3);
-            $new_link_start = $link[1];
-            $new_link_end = $link[2];
-            $new_link = "/".$new_link_start."-2019/".$new_link_end;
-            echo '<pre>';
-            print_r($new_link);
-            echo '</pre>';
-            //LocalRedirect($new_link);
-        }
-
-    }
+if ($pos === false) {
+    $link = explode('/', $_SERVER['REQUEST_URI'], 3);
+    $new_link_start = $link[1];
+    $new_link_end = $link[2];
+    $new_link = "/".$new_link_start."-2019/".$new_link_end;
+    LocalRedirect($new_link);
 }
-
 ?>
