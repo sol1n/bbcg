3c89548f82581cda034a54f159db79dc9519e116
Unknown <dr.nightingale@mail.ru>
2019-03-21 17:52:25 +0800

    new redirect variant

--- www/summits/redirect.php
+++ www/summits/redirect.php
@@ -1,19 +1,24 @@
 <?
-//если год саммита не указан в url перенаправлять на страницу текущего года
+// массив названий саммитов
+$arr_summits = array(
+    'academy-customer-experience-officer',
+    'shop-forever',
+    'akademiya-riteyla',
+    'online-retail-russia',
+    'food-business-russia',
+    'digital-technologies-forum',
+    'retail-business-russia',
+    'fashion-retail-russia',
+    'commercial-director-forum',
+    'diy-household-retail-russia',
+);
 
+//если год саммита не указан в url перенаправлять на страницу текущего года
 $link = explode('/', $_SERVER['REQUEST_URI'], 3);
 $new_link_start = $link[1];
 $new_link_end = $link[2];
 
-$file_path = explode('/', $_SERVER['REAL_FILE_PATH'], 3);
-
-$haystack = $new_link_start;
-$needle = '20';
-$pos = strripos($haystack, $needle);
-
-if (($pos === false) && ($new_link_start != '') && ($file_path[1] == 'summits') && ($new_link_start != 'login')
- && ($new_link_start != 'academy') && ($new_link_start != 'registration')  && ($new_link_start != 'retail')
-  && ($new_link_start != 'about') && ($new_link_start != 'eula')) {
+if(in_array($new_link_start, $arr_summits)){
     $new_link = "/".$new_link_start."-2019/".$new_link_end;
     LocalRedirect($new_link);
 }
