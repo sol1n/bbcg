dea518b5ef5da83eddfb3a1abd4f2ab00e5f2929
Unknown <dr.nightingale@mail.ru>
2019-08-13 15:15:56 +0800

    delete debug info

--- www/bitrix/templates/.default/components/bitrix/news.list/events-page/template.php
+++ www/bitrix/templates/.default/components/bitrix/news.list/events-page/template.php
@@ -176,7 +176,6 @@ function cmp($a, $b){
                                                     </div>
                                                     <div class="program-table-event-description">
                                                         <?=$event['detail']?>
-                                                        <pre><?print_r($event['detail']);?></pre>]
                                                     </div>
                                                     <? if ($event['speakers']): ?>
                                                         <div class="program-table-event-speakers">
